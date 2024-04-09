c91d08dd999601a422bb7f4873d7c1b800c7ce67
Sean Balbale <sean.balbale@gmail.com>
2024-04-10 00:36:51 +0100

    refactored to add pixel class

--- project2/src/main/java/uk/ac/nulondon/Image.java
+++ project2/src/main/java/uk/ac/nulondon/Image.java
@@ -8,56 +8,35 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Random;
 
+import static java.lang.Math.sqrt;
+
 public class Image {
 
-    private ArrayList<ArrayList<ArrayList<Integer>>> image = new ArrayList<ArrayList<ArrayList<Integer>>>();
+    private ArrayList<ArrayList<Pixel>> image = new ArrayList<>();
     public int count = 0;
     public int undoCount = 0;
     public int operationCount = 0;
     public int selectedColumn = -1;
     public String filePath = "";
 
-
-
     public Image(String filePath) throws IOException {
-        // Constructor
-        // This is where you would open the file and read the image then turn the image into a 2d array of tuples
-        File input = new File(filePath);  // Create file object
-        BufferedImage img = ImageIO.read(input);  // Read image from file
-        int width = img.getWidth();  // Get width of image
-        int height = img.getHeight();  // Get height of image
+        File input = new File(filePath);
+        BufferedImage img = ImageIO.read(input);
+        int width = img.getWidth();
+        int height = img.getHeight();
         this.filePath = filePath;
-        System.out.println("Width: " + width + " Height: " + height);  // Print width and height of image
+        System.out.println("Width: " + width + " Height: " + height);
 
-
-
-        // This loops through the image pixel by pixel
-        // we will read in the original color then change
-        // the color and store into new buffer
         for (int row = 0; row < height; row++) {
-            ArrayList<ArrayList<Integer>> rowArray = new ArrayList<ArrayList<Integer>>();
+            ArrayList<Pixel> rowArray = new ArrayList<>();
             this.image.add(rowArray);
             for (int col = 0; col < width; col++) {
-//                System.out.println("Reading pixel at: " + row + " " + col);
-
-                // Retrieving contents of a pixel
                 int pixel = img.getRGB(col, row);
-
-                // Creating a Color object from pixel value
                 Color originalColor = new Color(pixel);
-
-                // Retrieving the Red Green and Blue values
                 int red = originalColor.getRed();
                 int green = originalColor.getGreen();
                 int blue = originalColor.getBlue();
-
-                // Alter the color and then store in new image buffer
-                ArrayList<Integer> pixelArray = new ArrayList<Integer>();
-                pixelArray.add(red);
-                pixelArray.add(green);
-                pixelArray.add(blue);
-
-                this.image.get(row).add(pixelArray);
+                this.image.get(row).add(new Pixel(red, green, blue));
             }
         }
     }
@@ -69,23 +48,21 @@ public class Image {
     }
 
     public void printImage() {
-        // This is where you would print the image to the console
-        for (int row = 0; row < this.image.size(); row++) {
-            for (int col = 0; col < this.image.get(row).size(); col++) {
-                System.out.print(this.image.get(row).get(col));
+        for (ArrayList<Pixel> row : this.image) {
+            for (Pixel pixel : row) {
+                System.out.print(pixel.toString());
             }
             System.out.println();
         }
     }
 
     public int findBluestColumn() {
-        // This is where you would find the bluest column in the image
         int bluestColumn = 0;
         int bluestColumnValue = 0;
         for (int col = 0; col < this.image.get(0).size(); col++) {
             int columnValue = 0;
-            for (int row = 0; row < this.image.size(); row++) {
-                columnValue += this.image.get(row).get(col).get(2);
+            for (ArrayList<Pixel> row : this.image) {
+                columnValue += row.get(col).getBlue();
             }
             if (columnValue > bluestColumnValue) {
                 bluestColumnValue = columnValue;
@@ -96,37 +73,29 @@ public class Image {
     }
 
     public void changeColumnColor(int column, int red, int green, int blue) {
-        // This is where you would change the color of a column
-        for (int row = 0; row < this.image.size(); row++) {
-            for (int col = 0; col < this.image.get(row).size(); col++) {
-                if (col == column) {
-                    this.image.get(row).get(col).set(0, red);
-                    this.image.get(row).get(col).set(1, green);
-                    this.image.get(row).get(col).set(2, blue);
-                }
-            }
+        for (ArrayList<Pixel> row : this.image) {
+            Pixel pixel = row.get(column);
+            pixel.setRed(red);
+            pixel.setGreen(green);
+            pixel.setBlue(blue);
         }
     }
 
     public void removeColumn(int column) {
-        // This is where you would remove a column from the image
-        for (int row = 0; row < this.image.size(); row++) {
-            this.image.get(row).remove(column);
+        for (ArrayList<Pixel> row : this.image) {
+            row.remove(column);
         }
     }
 
     public void saveImage(String filePath) throws IOException {
         ++this.count;
-        // This is where you would save the image to a file
         int width = this.image.get(0).size();
         int height = this.image.size();
         BufferedImage newImage = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
         for (int row = 0; row < height; row++) {
             for (int col = 0; col < width; col++) {
-                int red = this.image.get(row).get(col).get(0);
-                int green = this.image.get(row).get(col).get(1);
-                int blue = this.image.get(row).get(col).get(2);
-                Color newColor = new Color(red, green, blue);
+                Pixel pixel = this.image.get(row).get(col);
+                Color newColor = new Color(pixel.getRed(), pixel.getGreen(), pixel.getBlue());
                 newImage.setRGB(col, row, newColor.getRGB());
             }
         }
@@ -135,17 +104,7 @@ public class Image {
     }
 
     public void undo() throws IOException {
-//        if (this.operationCount == 0) {
-//            System.out.println("No operations to undo");
-//            return;
-//        }
-        // This is where you would undo the last operation
         this.undoCount++;
-//        Using d calculations to get the correct image
-//        System.out.println("count: " + this.count + " undoCount: " + this.undoCount + " calculated count: " + (this.count-(2*this.undoCount)-this.undoCount));
-//        Image img = new Image("Output/tempIMG_0" + (this.count-(2*this.undoCount)-this.undoCount) + ".png");
-
-//        Using confirm calculations
         if((this.count-(2*this.undoCount)-this.undoCount) == -1){
             Image img = new Image(this.filePath);
             this.image = img.image;
@@ -154,11 +113,9 @@ public class Image {
             Image img = new Image("Output/tempIMG_0" + (this.count-(2*this.undoCount)-this.undoCount) + ".png");
             this.image = img.image;
         }
-
-
     }
+
     public void backOne() throws IOException {
-        // This is where you would undo the last operation
         if (this.count - 2 < 0){
             Image img = new Image(this.filePath);
             this.image = img.image;
@@ -167,8 +124,38 @@ public class Image {
             Image img = new Image("Output/tempIMG_0" + (this.count-2) + ".png");
             this.image = img.image;
         }
-
-
     }
+
+//  The brightness of a Pixel is the average of the RGB values of the color of a
+//  given pixel.
+    public int brightness(Pixel pixel) {
+        return (pixel.getRed() + pixel.getGreen() + pixel.getBlue()) / 3;
+    }
+
+//    𝐻𝑜𝑟𝑖𝑧𝐸𝑛𝑒𝑟𝑔𝑦(𝐸) = (𝑏𝑟(𝐴) + 2𝑏𝑟(𝐷) + 𝑏𝑟(𝐺)) − (𝑏𝑟(𝐶) + 2𝑏𝑟(𝐹) + 𝑏𝑟(𝐼))
+//    𝑉𝑒𝑟𝑡𝐸𝑛𝑒𝑟𝑔𝑦(𝐸) = (𝑏𝑟(𝐴) + 2𝑏𝑟(𝐵) + 𝑏𝑟(𝐶)) − (𝑏𝑟(𝐺) + 2𝑏𝑟(𝐻) + 𝑏𝑟(𝐼))
+//    Energy(E) = sqrt(HorizontalEnergy(E)^2 + VerticalEnergy(E)^2)
+    public double energy(int row, int col) {
+        int width = this.image.get(0).size();
+        int height = this.image.size();
+        int left = (col - 1 + width) % width;
+        int right = (col + 1) % width;
+        int up = (row - 1 + height) % height;
+        int down = (row + 1) % height;
+        Pixel A = this.image.get(up).get(left);
+        Pixel B = this.image.get(up).get(col);
+        Pixel C = this.image.get(up).get(right);
+        Pixel D = this.image.get(row).get(left);
+        Pixel E = this.image.get(row).get(col);
+        Pixel F = this.image.get(row).get(right);
+        Pixel G = this.image.get(down).get(left);
+        Pixel H = this.image.get(down).get(col);
+        Pixel I = this.image.get(down).get(right);
+        int horizontalEnergy = Math.abs(brightness(A) + 2 * brightness(D) + brightness(G) - brightness(C) - 2 * brightness(F) - brightness(I));
+        int verticalEnergy = Math.abs(brightness(A) + 2 * brightness(B) + brightness(C) - brightness(G) - 2 * brightness(H) - brightness(I));
+        return sqrt(horizontalEnergy * horizontalEnergy + verticalEnergy * verticalEnergy);
+    }
+
+
 
 }
