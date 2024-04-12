180e8d6a4e367f6d8298425f6a7fc677d460d115
Sean Balbale <sean.balbale@gmail.com>
2024-04-12 12:07:08 +0100

    Update Main.java

--- project2/src/main/java/uk/ac/nulondon/Main.java
+++ project2/src/main/java/uk/ac/nulondon/Main.java
@@ -1,6 +1,14 @@
 package uk.ac.nulondon;
 
 public class Main {
+    /**
+     * The main method of the application.
+     * It creates an Image object from a file path and prints the color of each pixel in the image.
+     * If an exception occurs during the creation of the Image object or during the printing of the image,
+     * the exception is caught and its stack trace is printed.
+     *
+     * @param args The command line arguments.
+     */
     public static void main(String[] args) {
         try {
             Image image = new Image("src/main/resources/8x8 Images/beach.png");
